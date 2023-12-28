f9ca9fd8171a0abd213a3335791db98e7ee53253
Alexander Ng <alexng353@gmail.com>
2023-12-28 02:21:50 -0800

    save

--- src/guards/user.rs
+++ src/guards/user.rs
@@ -1,10 +1,12 @@
 use crate::structs::User;
 use anyhow::{anyhow, Error};
 use rocket::{
-    data::Outcome,
     http::Status,
+    request::Outcome,
     request::{self, FromRequest, Request},
 };
+
+use pgp::{composed::message::Message, Deserializable};
 
 #[rocket::async_trait]
 impl<'r> FromRequest<'r> for User {
@@ -15,5 +17,17 @@ impl<'r> FromRequest<'r> for User {
             Some(authorization) => authorization,
             None => return Outcome::Error((Status::Unauthorized, ())),
         };
+
+        let (id, signed_challenge) = match authorization.split_once(':') {
+            Some((id, signed_challenge)) => (id, signed_challenge),
+            None => return Outcome::Error((Status::Unauthorized, ())),
+        };
+
+        let signed_challenge = Message::from_string(&signed_challenge).unwrap().0;
+
+        let challenge =
+            std::str::from_utf8(&signed_challenge.get_content().unwrap().unwrap()).unwrap();
+
+        let challenge_date = Date
     }
 }
